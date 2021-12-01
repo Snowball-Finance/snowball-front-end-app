e17140b637063ad08472195b15df6b553c86e20b
mohsen <mhsnprvr4@gmail.com>
2021-12-02 02:51:45 +0330

    same

--- src/app/containers/BlockChain/saga.ts
+++ src/app/containers/BlockChain/saga.ts
@@ -7,17 +7,12 @@ import { selectAccountDomain, selectLibraryDomain } from "./containers/Web3/sele
 import { BlockChainActions } from "./slice";
 
 import { ethers } from 'ethers'
-import { CONTRACTS } from "config";
-import SNOWBALL_ABI from 'libs/abis/snowball.json'
 import { BNToFloat } from "utils/format";
 
 export function* getBalance(action: { type: string, payload: ethers.Contract }) {
   const account = yield select(selectAccountDomain)
-  const provider = yield select(selectPrivateProviderDomain)
-  const snobContract = new ethers.Contract(CONTRACTS.SNOWBALL, SNOWBALL_ABI, provider)
   try {
-    console.log('calling for balance')
-    const response = yield call(snobContract['balanceOf(address)'], account)
+    const response = yield call(action.payload['balanceOf(address)'], account)
     const snowballBalanceValue = BNToFloat(response, 18);
     console.log(snowballBalanceValue)
   } catch (error) {
