90174122d491ec30faf12cb3915baacaaca0d19a
mohsen <mhsnprvr4@gmail.com>
2021-12-13 23:02:40 +0330

    same

--- src/app/containers/pages/Home/index.tsx
+++ src/app/containers/pages/Home/index.tsx
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { memo } from 'react';
+import React from 'react';
 import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
 import { HomePageReducer, sliceKey } from './slice';
 import { homePageSaga } from './saga';
@@ -12,11 +12,10 @@ import { ContainedButton } from "app/components/common/buttons/containedButton";
 import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
 import { AppPages } from "app/constants";
-import { matomo } from "app/containers/Analytics/slice";
 
 interface Props { }
 
-export const HomePage = memo((props: Props) => {
+export const HomePage = (props: Props) => {
   useInjectReducer({ key: sliceKey, reducer: HomePageReducer });
   useInjectSaga({ key: sliceKey, saga: homePageSaga });
 
@@ -31,4 +30,4 @@ export const HomePage = memo((props: Props) => {
       Go To Example
     </ContainedButton>
   );
-});
+};
