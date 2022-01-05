7bf89c37dbd2c3bc537376ed125e6b630a41e886
mohsen <mhsnprvr4@gmail.com>
2022-01-06 00:21:42 +0330

    throw error when something is missing in governance module

--- src/app/containers/Governance/index.tsx
+++ src/app/containers/Governance/index.tsx
@@ -4,6 +4,25 @@ import { selectGovernanceTokenContract } from "./selectors"
 import { GovernanceActions, useGovernanceSlice } from "./slice"
 
 export const Governance=({governanceTokenABI}:{governanceTokenABI:any})=>{
+
+const variables={
+  MINIMUM_TOKEN_FOR_VOTING:process.env.REACT_APP_MINIMUM_TOKEN_FOR_VOTING,
+  MINIMUM_VOTING_PERIOD:process.env.REACT_APP_MINIMUM_VOTING_PERIOD,
+  MAXIMUM_VOTING_PERIOD:process.env.REACT_APP_MAXIMUM_VOTING_PERIOD,
+  MINIMUM_VOTING_PERIOD_UNIT:process.env.REACT_APP_MINIMUM_VOTING_PERIOD_UNIT,
+  GOVERNANCE_INFO_LINK:process.env.REACT_APP_GOVERNANCE_INFO_LINK,
+  GOVERNANCE_TOKEN_NAME:process.env.REACT_APP_GOVERNANCE_TOKEN_NAME,
+  GOVERNANCE_TOKEN_CONTRACT_ADDRESS:process.env.REACT_APP_GOVERNANCE_TOKEN_CONTRACT_ADDRESS,
+  GOVERNANCE_TOKEN_LOGO_ADDRESS:process.env.REACT_APP_GOVERNANCE_TOKEN_LOGO_ADDRESS,
+}
+
+for(let key in variables){
+  if(!variables[key]){
+    throw new Error(`REACT_APP_${key} is not set in .env for the governance`)
+  }
+}
+
+
   useGovernanceSlice()
   const dispatch = useDispatch()
   const governanceToken=useSelector(selectGovernanceTokenContract)
