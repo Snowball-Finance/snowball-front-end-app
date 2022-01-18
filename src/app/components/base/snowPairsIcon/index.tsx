95bc76e366a3162047154b08ef6b6a4279c01419
mohsen <mhsnprvr4@gmail.com>
2022-01-18 04:23:04 +0330

    same

--- src/app/components/base/snowPairsIcon/index.tsx
+++ src/app/components/base/snowPairsIcon/index.tsx
@@ -3,9 +3,6 @@ import { generateIconSrcByAddress } from "common/generateIconSrcByAddress";
 import orderBasePair from "common/orderBasePair";
 
 const Wrapper = styled("div")<any>(({ theme, size, flat }) => ({
-
-
-  
   width: `${size || 50}px`,
   height: `${size || 50}px`,
   borderRadius: "50%",
