d943d1795b354f824e8fe37d3b5bc4c7e58d6c1c
mohsen <mhsnprvr4@gmail.com>
2022-02-01 02:50:08 +0330

    same

--- src/app/containers/pages/StakingPage/slice.ts
+++ src/app/containers/pages/StakingPage/slice.ts
@@ -4,10 +4,12 @@ import { createSlice } from "store/toolkit";
 import { useInjectReducer, useInjectSaga } from "store/redux-injectors";
 
 import { stakingPageSaga } from "./saga";
+import { getDayOffset } from "app/containers/BlockChain/Governance/Staking/helpers/date";
 
 // The initial state of the StakingPage container
 export const initialState: ContainerState = {
   enteredMainTokenToStake: "",
+  selectedEpoch: undefined,
 };
 
 const stakingPageSlice = createSlice({
@@ -16,6 +18,27 @@ const stakingPageSlice = createSlice({
   reducers: {
     setEnteredMainTokenToStake(state, action: PayloadAction<string>) {
       state.enteredMainTokenToStake = action.payload;
+    },
+    setSelectedEpoch(state, action: PayloadAction<number>) {
+      let selectedEpoch;
+      switch (action.payload) {
+        case 1:
+          selectedEpoch = getDayOffset(new Date(), 7);
+          break;
+        case 2:
+          selectedEpoch = getDayOffset(new Date(), 30);
+          break;
+        case 3:
+          selectedEpoch = getDayOffset(new Date(), 364);
+          break;
+        case 4:
+          selectedEpoch = getDayOffset(new Date(), 365 * 2);
+          break;
+        default:
+          selectedEpoch = getDayOffset(new Date(), 7);
+          break;
+      }
+      state.selectedEpoch = selectedEpoch;
     },
   },
 });
